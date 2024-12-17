65a6cc4bc9ba7bc2211656fcff78c53db7c2ae1e
mrsvolodya <vovamursky@gmail.com>
2024-12-17 14:22:00 +0200

    json array

--- src/server.js
+++ src/server.js
@@ -37,6 +37,42 @@ app.delete("/movies/:id", (req, res) => {
   return res.status(404).json({ message: "Movie not found" });
 });
 
+app.patch("movies/:id", (req, res) => {
+  const { id } = req.params;
+  const movieIndex = movieData.findIndex((movie) => movie.id === parseInt(id));
+
+  if (findIndex !== -1) {
+    const {
+      title,
+      image,
+      rating,
+      releaseDate,
+      description,
+      actors,
+      director,
+      genre,
+    } = req.body;
+
+    const updateMovie = {
+      ...movieData[movieIndex],
+      title: title || movieData[movieIndex].title,
+      image: image || movieData[movieIndex].image,
+      rating: rating || movieData[movieIndex].rating,
+      releaseDate: releaseDate || movieData[movieIndex].releaseDate,
+      description: description || movieData[movieIndex].description,
+      actors: actors || movieData[movieIndex].actors,
+      director: director || movieData[movieIndex].director,
+      genre: genre || movieData[movieIndex].genre,
+    };
+
+    movieData[movieIndex] = updateMovie;
+
+    return res.status(200).json(updateMovie);
+  }
+
+  return res.status(404).json({ message: "Movie not found" });
+});
+
 app.listen(3000, () => {
   console.log("APP IS LISTENING! http://localhost:3000");
 });
