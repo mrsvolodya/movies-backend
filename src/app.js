64224728258346fa62c8fe57467fbe020d4aabee
mrsvolodya <volodyamurskyi@gmail.com>
2025-06-21 13:52:10 +0300

    Fix port configuration

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-import express from "express";
 import cors from "cors";
+import "dotenv/config";
+import express from "express";
 import { router as movieRouter } from "./routes/todo.route.js";
 const app = express();
 
 app.use(cors());
 
 app.use("/", express.json(), movieRouter);
+const PORT = process.env.PORT || 3005;
 
-app.listen(3005, () => {});
+app.listen(PORT, () => {});
